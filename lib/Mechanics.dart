5d6f92e7a0a8353049150ee3ba2f8215b7bb2b5f
uwasejeannine <jeannineuwasee@gmail.com>
2024-07-01 13:06:39 +0200

    add

--- lib/Mechanics.dart
+++ lib/Mechanics.dart
@@ -4,125 +4,182 @@ class MechanicsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true, // Extend the body behind the app bar
       appBar: AppBar(
-        title: Text('Mechanics'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2, // Two cards per row
-            crossAxisSpacing: 16.0,
-            mainAxisSpacing: 16.0,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: CircleAvatar(
+            radius: 20,
+            backgroundImage:
+                AssetImage("images/five.png"), // Replace with actual image
           ),
-          itemCount: 10,
-          itemBuilder: (BuildContext context, int index) {
-            return _buildMechanicCard(context);
-          },
+        ),
+        title: const Text(
+          'Mutabazi John',
+          style: TextStyle(color: Colors.black),
+        ),
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("images/group.png"),
+              alignment: Alignment.topRight,
+              fit: BoxFit.fitWidth, // Adjust the fit as per your requirement
+            ),
+          ),
         ),
       ),
-    );
-  }
-
-  Widget _buildMechanicCard(BuildContext context) {
-    return Card(
-      child: Stack(
-        children: [
-          Image.network(
-            'https://via.placeholder.com/100',
-            fit: BoxFit.cover,
-            width: double.infinity,
-            height: double.infinity,
-          ),
-          const Positioned(
-            top: 8.0,
-            left: 8.0,
-            child: RotatedBox(
-              quarterTurns: 3,
-              child: FittedBox(
-                child: SizedBox(
-                  width: 100.0,
-                  child: Text(
-                    'Mechanic Name',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-          const Positioned(
-            bottom: 8.0,
-            left: 8.0,
-            child: FittedBox(
-              child: SizedBox(
-                width: 100.0,
-                child: Text(
-                  'Short description about the mechanic.',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: 8.0,
-            right: 8.0,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => BookMechanicPage()),
-                );
-              },
-              child: Text('Book'),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class BookMechanicPage extends StatelessWidget {
-  const BookMechanicPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Book Mechanic'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: Container(
+        color: Colors.white, // White background for the rest of the body
+        child: Stack(
           children: [
-            const Text(
-              'Mechanic Name',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image:
+                      AssetImage("images/two.png"), // Replace with actual image
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-            const SizedBox(height: 16),
-            const Text(
-              'Short description about the mechanic.',
-              style: TextStyle(fontSize: 16),
+            Container(
+              color: Colors.white.withOpacity(0.9),
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: kToolbarHeight + 16), // Space for the AppBar
+                  const Text(
+                    'Available Mechanics',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 16),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: _buildMechanicCard(
+                          'Kate Marian',
+                          'Car Mechanics',
+                          '10 AM\nDec 23',
+                          'images/one.png', // Replace with actual image
+                        ),
+                      ),
+                      SizedBox(width: 16),
+                      Expanded(
+                        child: _buildMechanicCard(
+                          'Paul Mitchell',
+                          'Car Mechanics',
+                          '1:30 AM\nDec 23',
+                          'images/four.png', // Replace with actual image
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 16),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: ElevatedButton(
+                          child: Text('Request A mechanic'),
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.lightBlue,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 16),
+                      Expanded(
+                        child: ElevatedButton(
+                          child: Text('Compare Prices'),
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.grey[300],
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  _buildMechanicListItem(
+                      'Mukamurisa Leonsi', 'Cra Mechanic', 'images/three.png'),
+                  _buildMechanicListItem('Dr. Mukamurisa Leonsi',
+                      'Car Mechanic', 'images/two.png'),
+                ],
+              ),
             ),
-            const SizedBox(height: 32),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home, color: Colors.white), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.list, color: Colors.white), label: 'List'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person, color: Colors.white), label: 'Profile'),
+        ],
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white60,
+        backgroundColor: Colors.black,
+      ),
+    );
+  }
+
+  Widget _buildMechanicCard(
+      String name, String specialty, String time, String imagePath) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+      elevation: 3,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            CircleAvatar(
+              radius: 30, // Specify a radius for proper sizing
+              backgroundImage:
+                  AssetImage(imagePath), // Use the passed image path
+            ),
+            const SizedBox(height: 8),
+            Text(name, style: const TextStyle(fontWeight: FontWeight.bold)),
+            Text(specialty),
+            Text(time, textAlign: TextAlign.center),
+            SizedBox(height: 8), // Add some space before the button
             ElevatedButton(
-              onPressed: () {
-                // Implement booking logic
-              },
-              child: const Text('Book Mechanic'),
+              child: Text('View'),
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.lightBlue,
+                foregroundColor: Colors.white, // Ensure text is visible
+              ),
             ),
           ],
         ),
       ),
     );
   }
+
+  Widget _buildMechanicListItem(
+      String name, String specialty, String imagePath) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundImage: AssetImage(imagePath), // Use the passed image path
+      ),
+      title: Text(name, style: TextStyle(fontWeight: FontWeight.bold)),
+      subtitle: Text(specialty),
+    );
+  }
 }
+
+void main() => runApp(MaterialApp(
+      home: MechanicsPage(),
+    ));
